618cc9b3d93e06fee6b471af533be563d5b513b5
Gabriel McDonald <gmcdonald@axta.com>
2025-01-06 18:37:59 -0500

    working on some graph algorithms

--- src/adjacency_map.rs
+++ src/adjacency_map.rs
@@ -1,14 +1,14 @@
+use crate::common::{IsGraph, Ray};
 use std::collections::HashMap;
 use std::hash::Hash;
-use crate::common::{IsGraph, Ray};
 
-pub struct AdjacencyMap<N: Copy+Eq+Hash, E: Ord+Copy> {
-    graph: HashMap<N,HashMap<N,E>>,
+pub struct AdjacencyMap<N: Copy + Eq + Hash, E: Ord + Copy> {
+    graph: HashMap<N, HashMap<N, E>>,
     is_directional: bool,
-    is_cyclical: bool
+    is_cyclical: bool,
 }
 
-impl<N: Copy+Eq+Hash, E: Ord+Copy> IsGraph<N, E> for AdjacencyMap<N, E> {
+impl<N: Copy + Eq + Hash, E: Ord + Copy> IsGraph<N, E> for AdjacencyMap<N, E> {
     fn add_bidirectional_edge(&mut self, i: N, j: N, val: E) {
         if self.graph.contains_key(&i) && self.graph.contains_key(&j) {
             self.graph.get_mut(&i).unwrap().insert(j, val);
@@ -23,15 +23,15 @@ impl<N: Copy+Eq+Hash, E: Ord+Copy> IsGraph<N, E> for AdjacencyMap<N, E> {
         }
     }
 
-    fn new(nodes: Box<dyn Iterator<Item=N>>) -> Self {
-        let mut empty_map = HashMap::<N, HashMap<N,E>>::new();
-        for n in nodes{
-            empty_map.insert(n,HashMap::<N,E>::new());
+    fn new(nodes: Box<dyn Iterator<Item = N>>) -> Self {
+        let mut empty_map = HashMap::<N, HashMap<N, E>>::new();
+        for n in nodes {
+            empty_map.insert(n, HashMap::<N, E>::new());
         }
-        Self{
+        Self {
             graph: empty_map,
             is_directional: false,
-            is_cyclical: false
+            is_cyclical: false,
         }
     }
 
@@ -41,8 +41,8 @@ impl<N: Copy+Eq+Hash, E: Ord+Copy> IsGraph<N, E> for AdjacencyMap<N, E> {
 
     fn check_directional(&mut self) {
         for i in self.graph.keys() {
-            for j in self.graph.keys(){
-                if self.graph[i][j] != self.graph[j][i]{
+            for j in self.graph.keys() {
+                if self.graph[i][j] != self.graph[j][i] {
                     self.is_directional = true;
                     return;
                 }
@@ -52,36 +52,41 @@ impl<N: Copy+Eq+Hash, E: Ord+Copy> IsGraph<N, E> for AdjacencyMap<N, E> {
     }
 
     fn get_targets(&self, node: N) -> Vec<Ray<N, E>> {
-        if let Some(targets) = self.graph.get(&node){
-            targets.iter().map(|(&k,&v)| Ray::new(k,v)).collect()
-        } else{
+        if let Some(targets) = self.graph.get(&node) {
+            targets.iter().map(|(&k, &v)| Ray::new(k, v)).collect()
+        } else {
             Vec::new()
         }
     }
 
     fn get_sources(&self, node: N) -> Vec<Ray<N, E>> {
-        self.graph.iter().filter_map(
-            |(k,v)| if let Some(&s) = v.get(&node){
-                Some(Ray::new(*k,s))
-            } else {None})
+        self.graph
+            .iter()
+            .filter_map(|(k, v)| {
+                if let Some(&s) = v.get(&node) {
+                    Some(Ray::new(*k, s))
+                } else {
+                    None
+                }
+            })
             .collect()
     }
 
     fn remove_bidirectional_edge(&mut self, i: N, j: N) {
-        if let Some(x) = self.graph.get_mut(&i){
+        if let Some(x) = self.graph.get_mut(&i) {
             x.remove(&j);
         }
-        if let Some(y) = self.graph.get_mut(&j){
+        if let Some(y) = self.graph.get_mut(&j) {
             y.remove(&i);
         }
     }
 
     fn remove_directional_edge(&mut self, from: N, to: N) {
-        if let Some(x) = self.graph.get_mut(&from){
+        if let Some(x) = self.graph.get_mut(&from) {
             x.remove(&to);
         }
     }
-    
+
     fn add_node(&mut self, n: N) {
         self.graph.insert(n, HashMap::new());
     }
@@ -90,4 +95,3 @@ impl<N: Copy+Eq+Hash, E: Ord+Copy> IsGraph<N, E> for AdjacencyMap<N, E> {
         self.graph.keys().cloned().collect()
     }
 }
-
--- src/common.rs
+++ src/common.rs
@@ -1,31 +1,31 @@
-
-pub(crate) struct Ray<P,T: Ord+Copy>{
-    pub(crate) point:P,
-    pub(crate) edge: T
+pub(crate) struct Ray<P, T: Ord + Copy> {
+    pub(crate) point: P,
+    pub(crate) edge: T,
 }
 
-impl<P,T: Ord+Copy> Ray<P,T>{
-    pub fn new(point:P, edge:T) -> Self{Self{point,edge}}
+impl<P, T: Ord + Copy> Ray<P, T> {
+    pub fn new(point: P, edge: T) -> Self {
+        Self { point, edge }
+    }
 }
 
-pub(crate) trait IsGraph<P:Copy+Eq, T: Ord+Copy>{
-    fn add_bidirectional_edge(&mut self, i:P, j:P, val: T);
-    fn add_directional_edge(&mut self, from:P, to:P, val:T);
+pub(crate) trait IsGraph<P: Copy + Eq, T: Ord + Copy> {
+    fn add_bidirectional_edge(&mut self, i: P, j: P, val: T);
+    fn add_directional_edge(&mut self, from: P, to: P, val: T);
 
-    fn new(nodes: Box<dyn Iterator<Item=P>>) -> Self;
+    fn new(nodes: Box<dyn Iterator<Item = P>>) -> Self;
     fn is_cyclical(&self) -> bool;
 
     fn check_directional(&mut self);
 
-    fn get_targets(&self, node: P) -> Vec<Ray<P,T>>;
+    fn get_targets(&self, node: P) -> Vec<Ray<P, T>>;
 
-    fn get_sources(&self, node:P)-> Vec<Ray<P,T>>;
+    fn get_sources(&self, node: P) -> Vec<Ray<P, T>>;
 
-    fn remove_bidirectional_edge(&mut self, i:P, j:P);
-    fn remove_directional_edge(&mut self, from:P, to:P);
-    
-    fn add_node(&mut self, node:P);
+    fn remove_bidirectional_edge(&mut self, i: P, j: P);
+    fn remove_directional_edge(&mut self, from: P, to: P);
+
+    fn add_node(&mut self, node: P);
 
     fn get_nodes(&self) -> Vec<P>;
 }
-
